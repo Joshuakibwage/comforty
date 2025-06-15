8b319d00d3c971ccb3f2a6dee0f9766a2533f0c9
Josh <joshuakibwage23@gmail.com>
2025-06-15 19:28:41 +0300

    feat: add delivery component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from '../../Components/Banner/Banner';
+import Delivery from '../../Components/Delivery/Delivery';
 
 
 const Home = () => {
@@ -8,10 +9,15 @@ const Home = () => {
       
       {/* banner component */}
       <div className="w-full min-h-[850px] bg-[#f0f2f3] flex items-center justify-center rounded-b-3xl">
-
         <Banner></Banner>
-        
       </div> 
+
+      {/* Delivery component */}
+      <div className="delivery_component w-full min-h-[#150px] ">
+        <Delivery></Delivery>
+      </div>
+
+
     </div>
   )
 }
