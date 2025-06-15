a9412045f117817e76b0c18a172a73a807982f34
Josh <joshuakibwage23@gmail.com>
2025-06-15 17:56:35 +0300

    feat: add carousel in hero component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Banner from '../../Components/Banner/Banner';
+
 
 const Home = () => {
   return (
-    <div>
-      Home page
+    <div className="">
+      
+      {/* banner component */}
+      <div className="w-full min-h-[850px] bg-[#f0f2f3] flex items-center justify-center rounded-b-3xl">
+
+        <Banner></Banner>
+        
+      </div> 
     </div>
   )
 }
